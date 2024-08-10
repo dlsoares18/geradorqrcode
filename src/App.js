a12c5f00af8130a896ea8686ecdd3577176dda8f
dlsoares18 <dlsoares18@gmail.com>
2024-08-10 22:23:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import QRCode from 'qrcode-react';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [ texto, setTexto ] = useState('');
+  const [ qr, setQr ] = useState('');
+
+  function handleClique (){
+    setQr(texto);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>
+        <h1>Gerar QRCode</h1>
+        <QRCode
+          className='qrcode'
+          value={qr}
+          size={256}
+        />
+      </div>
+      <div>
+        <input
+          className='input'
+          type='text'
+          placeholder='Insira a url...'
+          value={texto}
+          onChange={ (e) => setTexto(e.target.value) }
+        />
+      </div>
+      <div>
+        <button className='button' onClick={ handleClique }>
+          Gerar
+        </button>
+      </div>
     </div>
   );
 }
