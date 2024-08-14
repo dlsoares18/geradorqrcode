a6bd9b4b18672c2ef3d993b7879f85e08a130831
dlsoares18 <dlsoares18@gmail.com>
2024-08-14 23:59:31 +0100

    adjust generate qrcode

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import QRCode from 'qrcode-react';
 import { useState } from 'react'
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
 import './App.css';
 
 function App() {
 
-  const [ texto, setTexto ] = useState('');
-  const [ qr, setQr ] = useState('');
+  const [texto, setTexto] = useState('');
+  const [qrLink, setQrLink] = useState('');
 
-  function handleClique (){
-    setQr(texto);
+  function handleGenerate(link) {
+    QRCodeLink.toDataURL(link, {
+      width: 600,
+      margin: 3,
+    }, function (err, url) {
+      setQrLink(url)
+    })
+  }
 
+  function handleClique(e) {
+    setTexto(e.target.value)
+    handleGenerate(e.target.value)
   }
 
   return (
     <div className='container'>
-      <div>
-        <h1>Gerar QRCode</h1>
-        <QRCode
-          className='qrcode'
-          value={qr}
-          size={256}
-        />
-      </div>
-      <div>
-        <input
-          className='input'
-          type='text'
-          placeholder='Insira a url...'
-          value={texto}
-          onChange={ (e) => setTexto(e.target.value) }
-        />
-      </div>
-      <div>
-        <button className='button' onClick={ handleClique }>
-          Gerar
-        </button>
-      </div>
+      <h1>Gerar QRCode</h1>
+      <QRCode
+        value={texto}
+      />
+      <input
+        className='input'
+        type='text'
+        placeholder='Insira a url...'
+        value={texto}
+        onChange={(e) => handleClique(e)}
+      />
+      <a className='button' href={qrLink} download={`qrcode.png`}>Baixar QRCode</a>
     </div>
   );
 }
